52bb76008442455774f3527e0a6b95d2e979792e
Thong Bui <buiquangthong@gmail.com>
2020-06-14 10:43:36 +0700

    Add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NotfoundComponent } from './core/components/notfound/notfound.component';
 
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
-  { path: '**', component: NotfoundComponent }
+  { path: '**', loadChildren: () => import('./modules/notfound/notfound.module').then(m => m.NotfoundModule) }
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const COMMON_COMPONENTS = [
   HeaderComponent,
-  FooterComponent,
-  NotfoundComponent
+  FooterComponent
 ];
 
 
